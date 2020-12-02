7dc4b2539c0473d90226848bc4b74fc5b502f343
Manol Dimitrov <m.dimitrov@elsevier.com>
2020-12-02 22:27:48 +0000

    Create Day2.kt

---
+++ Day2.kt
@@ -0,0 +1,51 @@
+import java.io.File
+
+fun main() {
+    val values = readFile("day2.txt")
+
+    validate(values, "A")
+    validate(values, "B")
+}
+
+private fun validate(values: ArrayList<String>, part: CharSequence) {
+    var counter = 0
+
+    for (value in values) {
+        val line = value.split(" ").toTypedArray()
+
+        val rangeStart = line[0].split("-")[0].toInt()
+        val rangeEnd = line[0].split("-")[1].toInt()
+
+        val char = line[1].subSequence(0, 1)[0]
+        val password = line[2]
+
+        when (part) {
+            "A" -> {
+                val occurrences = password.filter { it == char }.count()
+
+                if (occurrences in rangeStart..rangeEnd) {
+                    counter++
+                }
+            }
+            "B" -> {
+                val firstCondition = password.toCharArray()[rangeStart - 1] == char
+                val secondCondition = password.toCharArray()[rangeEnd - 1] == char
+
+                if ((firstCondition || secondCondition) && !(firstCondition && secondCondition)) {
+                    counter++
+                }
+            }
+
+        }
+    }
+
+    println(counter)
+}
+
+fun readFile(filePath: String): ArrayList<String> {
+    val lines = arrayListOf<String>()
+
+    File(filePath).forEachLine { lines.add(it) }
+
+    return lines
+}
